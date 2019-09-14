9f26d14aa837673e1992af8b6dd63b8bc872288c
meecrub <15962474+meecrub@users.noreply.github.com>
2019-09-14 17:56:11 -0400

    Update myIncludeFunc.asm

--- nasm/myIncludeFunc.asm
+++ nasm/myIncludeFunc.asm
@@ -6,12 +6,12 @@ prints:				;printf(char* str)
   mov ebp, esp
     mov edi, [ebp + 8]    	;get string address
     mov esi, edi          	;save address for later len calculation
-    _prints_findNull:
+    .findNull:			;"." infront of laber = local scope label based on first global label(prints:). avoid name clash
       cmp byte [edi], 0x00    	;if \0
-      je _prints_done       	;exit loop
+      je .done       		;exit loop
       inc edi             	;else str++
-      jne _prints_findNull
-    _prints_done:
+      jne .findNull
+    .done:
       sub edi, esi        	;end of str addr(edi) - begin of str(esi) = strlen
   mov edx, edi	            	;set write arg3 strlen
   mov ecx, esi              	;set write arg2 buf
@@ -35,21 +35,21 @@ itoa:				;itoa(char* buf, int num)
   mov esi, 10       		;dividor
   mov edx, 0x00
   push edx         		;push null term
-  _itoa_divLoop:
+  .divLoop:
     xor edx, edx    		;clear remainder register
     div esi         		;now eax = quatian, edx=remainder
     add edx, 48     		;make num to char
     push edx        		;reverse push string to stack
     inc ecx         		;incrment strlen each divide
     cmp eax, 0      		;exit if quotian = 0
-    jnz _itoa_divLoop
+    jnz .divLoop
   inc ecx           		;inecx to include \0
   mov eax, ecx      		;return str index/len
-  _itoa_getNum:           	;loop reverse pop num char back
+  .getNum:           		;loop reverse pop num char back
     pop edx         		;get each char from rerar
     mov [edi], dl   		;move byte to index
     inc edi         		;increment index
-    loop _itoa_getNum     	;loop till ecx = 0
+    loop .getNum     		;loop till ecx = 0
   leave
   ret
 
@@ -58,12 +58,12 @@ strlen:				;strlen(char* str)
   mov ebp, esp
   mov esi, [ebp+8]		;str start address
   mov edi, esi			;copy to edi to find end of str
-  _strlen_findNull:		;locate null by inc str address
+  .findNull:			;locate null by inc str address
     cmp byte [edi], 0x00
-    je _strlen_done
+    je .done
     inc edi			;inc str sddress till find null
-    jne _strlen_findNull
-    _strlen_done:
+    jne .findNull
+    .done:
     sub edi, esi		;cal diff b/w start and end address
   mov eax, edi			;return strlen
   leave
@@ -80,18 +80,18 @@ atoi:				;atoi(char* str)
   mov esi, str                  ;str pointer
   xor edi, edi                  ;sum holder
   mov ebx, 10
-  _atoi_traverse:               ;outer loop traverse each base 10 index
+  .traverse:               ;outer loop traverse each base 10 index
     mov eax, 1                  ;multiplier seed
     push ecx                    ;save outer loop ecx
     dec ecx                     ;dec ecx for inner loop
-    _atoi_make_exponential:     ;build base 10 multiplier 10^3, 10^2, 10^1, etc
+    .make_exponential:     ;build base 10 multiplier 10^3, 10^2, 10^1, etc
       cmp ecx, 0
-      je _atoi_exp_done         ;break if idx = 0 else loop
+      je .done         ;break if idx = 0 else loop
       mul ebx
       dec ecx
       cmp ecx, 0
-      jne _atoi_make_exponential
-      _atoi_exp_done:
+      jne .make_exponential
+      .done:
     push eax                  ;save base 10 multiplier
     movzx edx, byte [esi]     ;get char
     inc esi                   ;point to next char
@@ -103,7 +103,7 @@ atoi:				;atoi(char* str)
     pop ecx                   ;get outer loop ecx
     dec ecx
     cmp ecx, 0
-    jne _atoi_traverse
+    jne .traverse
   mov eax, edi                  ;return str in int
   leave
   ret
