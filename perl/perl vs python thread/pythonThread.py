36d3d2a0c4bed0eae22f4b20bb351656b07b3daa
sleepyrepo <15962474+sleepyrepo@users.noreply.github.com>
2020-03-28 14:01:56 -0400

    Update pythonThread.py

--- perl/perl vs python thread/pythonThread.py
+++ perl/perl vs python thread/pythonThread.py
@@ -1,1 +1,27 @@
 
+#!/usr/bin/python
+
+import threading
+
+thrs = []
+count = 3                       #number of threads to create
+
+def dosth():
+  sth = ""
+  for i in range(1, 0xffffff):
+    sth += "%d"%i
+
+for i in range(1, count):
+  t = threading.Thread(target=dosth)
+  t.start()
+  thrs.append(t)
+
+for t in thrs:
+  t.join
+
+
+#run with
+#time python ./perlVsPyThread.py
+#real -> tome start to finish
+#user -> CPU time in user mode
+#sys -> CPU time in kernel mode
