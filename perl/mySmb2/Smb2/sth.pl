5290be288ed3a65854c5233902faf667b73b2514
sleepyrepo <15962474+sleepyrepo@users.noreply.github.com>
2020-12-26 10:49:28 -0500

    Delete sth.pl

--- perl/mySmb2/Smb2/sth.pl
+++
@@ -1,4 +0,0 @@
-require"./lmNtlm.pl";
-my ($user, $pass, $target, $challenge, $nonce) = ("mee", "pass1234", "WIN7", pack("H*","70c6a3229c48dc56"), pack("H*","0102030405060708"));
-printf"NTLM2 session response: %s\n", unpack("H*", ntlm2_session_response($pass, $challenge, $nonce));
-
